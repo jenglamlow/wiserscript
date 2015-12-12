a0ce5e86b0513362596c13457b53c16e0e65915c
Jeng Lam <jenglamlow@gmail.com>
2015-12-12 10:17:28 +0800

    Added menu h,l,u,x s[Num] action menu

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ import sys
 import re
 
 
+ACTION_VALID = 0
+ACTION_INVALID = 1
+ACTION_CONFLICT_BALL = 2
+
+
 def print_menu():
     print ("")
     print ("WISER GAME RECORDER")
@@ -10,43 +15,73 @@ def print_menu():
     print ("x. Exit")
 
 
-ACTION_VALID = 0
-ACTION_INVALID = 1
-ACTION_CONFLICT_BALL = 2
+def game_engine(action_string):
+    pass
 
 
-def validate(action_string):
-    if (action_string == 'x'):
+def action_validate(action_string):
+    if re.match('[hlux]|[s][\d]+', action_string):
             return ACTION_VALID
 
-    if (re.match('[rw][1-7][rw][1-7]|[rw][f]', action_string)):
-        if (len(action_string) == 4):
-            if(action_string[:2] == action_string[2:]):
+    if re.match('[rw][1-7][rw][1-7]|[rw][1-7][f]', action_string):
+        if len(action_string) == 4:
+            if action_string[:2] == action_string[2:]:
                 return ACTION_CONFLICT_BALL
         return ACTION_VALID
     else:
         return ACTION_INVALID
 
 
+def print_match_info():
+    print ("Usage: [STRIKER][TARGET]")
+    print ("  or:  [BALL][FOUL]")
+    print ("  or:  [OPTION]")
+    print ("    [STRIKER][TARGET]     [rw][1-7][rw][1-7]")
+    print ("    [BALL][FOUL]          [rw][1-7][f]")
+    print ("Example: r1w1, w1r2, r3r6, r2f")
+    print ("[OPTION]:")
+    print ("    h                     Help information")
+    print ("    l                     List all match sequence and info")
+    print ("    u                     Undo previous sequence")
+    print ("    s[Number]             Start edit from sequence Number, remove all the action after the sequence number")
+    print ("    x                     Exit")
+    print ("")
+
+
+def process_action_option(action_string):
+    if action_string == 'h':
+        print_match_info()
+    elif action_string == 'l':
+        print ("List information")
+    elif action_string == 'u':
+        print ("Undo")
+    elif action_string[0] == 's':
+        print ("start from")
+    else:
+        return False
+
+    return True
+
+
 def process_game():
     sequence = 1
     while(True):
         action = input("[" + str(sequence) + "]: ")
-        result = validate(action.lower())
-        if (result == ACTION_VALID):
-            if (action.lower() == "x"):
+        action = action.lower()
+        result = action_validate(action)
+        if result == ACTION_VALID:
+            if action == 'x':
                 break
+            if process_action_option(action):
+                continue
+            game_engine(action)
             sequence = sequence + 1
         else:
-            if (result == ACTION_INVALID):
+            if result == ACTION_INVALID:
                 print ('"' + action + '" is invalid action input')
             else:
                 print ('"' + action + '" is has same striker and target')
-            print ("Usage: [STRIKER][TARGET]")
-            print ("  or:  [BALL][FOUL]")
-            print ("  [STRIKER][TARGET]     [rRwW][1-7][rRwW][1-7]")
-            print ("  [BALL][FOUL]          [rRwW][fF]")
-            print ("Example: r1w1, w1r2, r3r6, rf")
+            print ('Try "h" to for more infomation.')
             print ("")
 
 
@@ -57,7 +92,7 @@ def new_game():
 
     while(True):
         game_name = input("Enter Game Name --> ")
-        if (re.match('^[\w\-_]+$', game_name)):
+        if re.match('^[\w\-_]+$', game_name):
             break
         else:
             print ('"' + game_name + '" is invalid filename format')
@@ -74,7 +109,7 @@ def errHandler():
     print ("Invalid Input..")
 
 
-action_map = {
+menu_map = {
     '1': new_game,
 }
 
@@ -84,8 +119,8 @@ def main(argv):
     while (True):
         print_menu()
         selection = input("-->")
-        if (selection.lower() != 'x'):
-            action_map.get(selection, errHandler)()
+        if selection.lower() != 'x':
+            menu_map.get(selection, errHandler)()
         else:
             break
 
