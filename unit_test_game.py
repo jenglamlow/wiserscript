391682e1c288590e0a890d21a69f73b3014ae5b9
Jeng Lam <jenglamlow@gmail.com>
2015-12-16 23:23:57 +0800

    Implemented draft test_first_locked unit_test

--- unit_test_game.py
+++ unit_test_game.py
@@ -7,31 +7,36 @@ class TestGame(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls._game = Game('game_test')
         red_list = ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7']
         white_list = ['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7']
-        cls._combo = map(''.join, itertools.chain(itertools.product(
-            red_list, white_list), itertools.product(white_list, red_list)))
+        cls._rw_combo = list(map(''.join, itertools.chain(itertools.product(
+            red_list, white_list), itertools.product(white_list, red_list))))
 
     def setUp(self):
-        self.game = Game('game_test_1')
+        self._game.reset()
 
     def tearDown(self):
         pass
 
     def test_first_locked(self):
-        self.game.process('r1w2')
+        for action in self._rw_combo:
+            target = action[2:]
+            self._game.process(action)
 
-        self.assertFalse(self.game.is_contesting('w2'))
-        self.assertTrue(self.game.is_first_lock('w2'))
-        self.assertFalse(self.game.is_second_lock('w2'))
-        self.assertFalse(self.game.is_eliminated('w2'))
+            self.assertFalse(self._game.is_contesting(target))
+            self.assertTrue(self._game.is_first_lock(target))
+            self.assertFalse(self._game.is_second_lock(target))
+            self.assertFalse(self._game.is_eliminated(target))
+
+            self._game.reset()
 
     def test_second_locked(self):
-        self.game.process('r1w2')
-        self.assertFalse(self.game.is_contesting('w2'))
-        self.assertTrue(self.game.is_first_lock('w2'))
-        self.assertFalse(self.game.is_second_lock('w2'))
-        self.assertFalse(self.game.is_eliminated('w2'))
+        self._game.process('r1w2')
+        self.assertFalse(self._game.is_contesting('w2'))
+        self.assertTrue(self._game.is_first_lock('w2'))
+        self.assertFalse(self._game.is_second_lock('w2'))
+        self.assertFalse(self._game.is_eliminated('w2'))
 
 
 if __name__ == '__main__':
